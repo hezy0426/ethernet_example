5631b86e107ff69fa5b3a4d9b151406b43a3be84
rayRayray <60754400+hezy0426@users.noreply.github.com>
2023-07-19 10:32:50 -0400

    Update webserver

--- components/webServer/include/webServer.h
+++ components/webServer/include/webServer.h
@@ -11,9 +11,5 @@
 
 httpd_handle_t start_webserver();
 void stop_webserver(httpd_handle_t server);
-void connect_handler(void *arg, esp_event_base_t event_base,
-                     int32_t event_id, void *event_data);
-void disconnect_handler(void *arg, esp_event_base_t event_base,
-                        int32_t event_id, void *event_data);
 
 #endif /* WEBSERVER_H */
